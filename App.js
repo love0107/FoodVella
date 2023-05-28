1498000642125ed4f1faa388c9faed72cf2e75d7
love0107 <lovlesh0107@gmail.com>
2023-05-28 13:04:34 +0530

    install react and react-dom

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from "rect";
+import ReactDOM , {createRoot} from "react-dom/client";
+
+
+
+const Title =()=>(
+<h1 id="title" key="h2">Food Vella</h1>
+);
+const HeaderComponent =()=> {
+    return (
+        <>
+        <Title/>
+        <div className="nav-items">
+            <ul>
+                <li>item1</li>
+                <li>item2</li>
+                <li>item3</li>
+            </ul>
+        </div>
+        </>
+    );
+
+}
+const Body=()=>{
+    return <div>
+        Body
+    </div>
+}
+
+const Footer =()=>{
+    return 
+    <div>
+        footer
+    </div>
+}
+const AppLayout =()=>{
+    return (
+     <>
+     <Header/>
+     <Body/>
+     <Foooter/>
+     </>
+    );
+}
